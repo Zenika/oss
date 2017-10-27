3d8997831c159615973b52516d8c492de9e330d9
Sehsyha <valentin.stern@zenika.com>
2017-10-27 11:45:19 +0200

    Add Main component with contributions list

--- oss-front/src/router/index.js
+++ oss-front/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Main from '@/components/Main';
+import Contributions from '@/components/Contributions';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Main',
-      component: Main,
+      name: 'Contributions',
+      component: Contributions,
     },
   ],
 });
