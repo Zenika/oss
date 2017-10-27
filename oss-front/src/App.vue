76580abb69ab362f466eb96d0394c182045c714c
Sehsyha <valentin.stern@zenika.com>
2017-10-27 11:17:59 +0200

    Add Header component

--- oss-front/src/App.vue
+++ oss-front/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <oss-header></oss-header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header';
+
 export default {
   name: 'app',
+  components: {
+    'oss-header': Header,
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ oss-front/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- Main Navbar-->
+  <header class="header">
+    <nav class="navbar">
+      <div class="container-fluid">
+        <div class="navbar-holder d-flex align-items-center justify-content-between">
+          <!-- Navbar Header-->
+          <div class="navbar-header">
+            <!-- Navbar Brand -->
+            <a href="index.html" class="navbar-brand">
+              <div class="brand-text brand-big hidden-lg-down">
+                <strong>Open Source</strong> <span>Contributions</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'oss-header',
+};
+</script>
+
+<style scoped>
+  .navbar-holder{
+    width: 100%;
+  }
+</style>
